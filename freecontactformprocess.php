f824fbcaac03b5a1ccca4491d1bc469b48842554
mihir225 <45895396+mihir225@users.noreply.github.com>
2019-01-01 18:10:45 +0530

    Update freecontactformprocess.php

--- freecontactformprocess.php
+++ freecontactformprocess.php
@@ -15,22 +15,22 @@ if(isset($_POST['email'])) {
     }
  
  
-    // validation expected data exists
-    if(!isset($_POST['first_name']) ||
-        !isset($_POST['last_name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['telephone']) ||
-        !isset($_POST['comments'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
-    }
+    if(!isset($_POST['Full_Name']) ||
+		!isset($_POST['Email_Address']) ||
+		!isset($_POST['Telephone_Number']) ||
+		!isset($_POST['Your_Message']) || 
+		!isset($_POST['AntiSpam'])		
+		) {
+		died('Sorry, there appears to be a problem with your form submission.');		
+	}
  
      
  
-    $first_name = $_POST['first_name']; // required
-    $last_name = $_POST['last_name']; // required
-    $email_from = $_POST['email']; // required
-    $telephone = $_POST['telephone']; // not required
-    $comments = $_POST['comments']; // required
+   $full_name = $_POST['Full_Name']; // required
+	$email_from = $_POST['Email_Address']; // required
+	$telephone = $_POST['Telephone_Number']; // not required
+	$comments = $_POST['Your_Message']; // required
+	$antispam = $_POST['AntiSpam']; // required
  
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
@@ -39,39 +39,32 @@ if(isset($_POST['email'])) {
     $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
   }
  
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$first_name)) {
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-  }
- 
-  if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-  }
+   if(strlen($full_name) < 2) {
+  	$error_message .= 'Your Name does not appear to be valid.<br />';
  
   if(strlen($comments) < 2) {
     $error_message .= 'The Comments you entered do not appear to be valid.<br />';
   }
+    
+    if($antispam <> $antispam_answer) {
+	$error_message .= 'The Anti-Spam answer you entered is not correct.<br />'; 
  
   if(strlen($error_message) > 0) {
     died($error_message);
   }
  
-    $email_message = "Form details below.\n\n";
+    $email_message = "Form details below.\r\n";
  
      
     function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
+	  $bad = array("content-type","bcc:","to:","cc:");
+	  return str_replace($bad,"",$string);
     }
  
-     
- 
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
+ $email_message .= "Full Name: ".clean_string($full_name)."\r\n";
+	$email_message .= "Email: ".clean_string($email_from)."\r\n";
+	$email_message .= "Telephone: ".clean_string($telephone)."\r\n";
+	$email_message .= "Message: ".clean_string($comments)."\r\n";
  
 // create email headers
 $headers = 'From: '.$email_from."\r\n".
