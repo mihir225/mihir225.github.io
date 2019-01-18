3ab151a837cb0a7c04e370502b09ac9ff74cd0dd
mihir225 <45895396+mihir225@users.noreply.github.com>
2019-01-18 11:05:48 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,202 @@
+<html><head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"/>
+<title><?php echo $_GET["n"]; ?> Wishing You</title>
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="<?php echo $_GET["n"]; ?> Wishing you." />
+    <meta property="og:url" content="" />
+    <meta property="og:description" content="Republic Day" />
+    <meta property="og:site_name" content="Create Indian Wishes" />
+    <meta property="og:image" content="/img/eid/spng.png">
+
+    <link type="text/css" rel="stylesheet" href="https://mihir225.github.io/wishstyle.css" media="screen,projection"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<style>
+input[type=name], select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 4px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+.footerbtn {
+  border-radius: 5px;
+  box-sizing: border-box;
+  padding: 5px;
+  position: fixed;
+  left:0px;
+  bottom:0px;
+  height:40px;
+  width:100%;
+  background:#34af23;
+  color: #ffffff;
+  font-size: 20px;
+  text-align: center;
+  text-decoration: none;
+}
+
+.footerbtn:hover {
+  text-decoration: none;
+    position: fixed;
+}
+@-webkit-keyframes jello {  from, 11.1%, to {    transform: none;  }
+  22.2% {    transform: skewX(-12.5deg) skewY(-12.5deg);  }
+  33.3% {    transform: skewX(6.25deg) skewY(6.25deg);  }
+  44.4% {    transform: skewX(-3.125deg) skewY(-3.125deg);  }
+  55.5% {    transform: skewX(1.5625deg) skewY(1.5625deg);  }
+  66.6% {    transform: skewX(-0.78125deg) skewY(-0.78125deg);  }
+  77.7% {    transform: skewX(0.390625deg) skewY(0.390625deg);  }
+  88.8% {    transform: skewX(-0.1953125deg) skewY(-0.1953125deg);  }}
+.jello {  -webkit-animation: jello 3s infinite;  transform-origin: center; -webkit-animation-delay:6s}
+@-webkit-keyframes hue {
+  from {    -webkit-filter: hue-rotate(0deg);  }
+  to {    -webkit-filter: hue-rotate(-360deg);  }}
+    .m1{position:fixed;left:1%; width:auto;height:100%;top:1%;color:#000;}
+    .m2{position:fixed;right:1%; width:auto;height:100%;top:1%;color:#000;}
+</style>
+
+</head>
+
+<body class="bg" id="new-year">
+<marquee class="m1" behavior="scroll" direction="up" scrolldelay="5">  <br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/123.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/124.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/123.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/123.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/124.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/123.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/123.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/124.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/123.png" height="129px" width="35px"/>
+</marquee>
+<marquee class="m2" behavior="scroll" direction="down" scrolldelay="5"><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/123.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/124.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/123.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/123.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/124.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/123.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/123.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/124.png" height="129px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/c.png" height="35px" width="35px"/><br><br>
+<img src="/img/eid/123.png" height="129px" width="35px"/>
+</marquee>
+<main id="new-year">
+ 
+<div class="">
+<div class="container">
+<div class="col-lg-12 col-sm-12 s12">
+<div class="row">
+<p class="center no-margin">
+</p>
+<div class="col-lg-12 s12 l6 offset-l3">
+<div class="card blue-grey darken-1 hoverable" id="bg-pic">
+<div class="card-content white-text">
+<p id="animated-example" class="animated pulse card-title center-align name head"><?php echo str_replace("_"," ",$_GET["n"]); ?></p>
+<p class="card-title center-align comic wishesed">
+<span class="deep-orange-text text-darken-2">W</span>
+<span class="green-text text-accent-4">I</span>
+<span class="blue-text text-lighten-1">S</span>
+<span class=" teal-text text-accent-3">H</span>
+<span class="red-text text-accent-3">I</span>
+<span class="lime-text">N</span>
+<span class="blue-text text-accent-3">G &nbsp;</span>
+<span class="lime-text">Y</span>
+<span class=" blue-text text-darken-1">O</span>
+<span class="lime-text text-accent-3">U</span></p>
+<h2 class="diwali jello"></h2>
+<p class="center">
+<img class="diwali jello" style="margin: 0 auto;display: block;" src="/img/eid/adv.jpg" height="250px" width="100%"/><br>
+
+<center></center>
+<font color="black"><big><b>
+
+अब तक जिसका खून न खौला,
+वो खून नहीं वो पानी है…
+जो देश के काम ना आये, वो बेकार जवानी है…
+बोलो भारत माता की जय..
+स्वतंत्र दिवस मुबारक हो!
+
+</b></big></font></p>
+<p class="name bdaywish right">-<?php echo $_GET["n"]; ?></p></br>
+<center>
+<div style="padding: 10px;"class="col-sm-12 ">
+<center><big><b><font color="#ffffff">Wish Your Friends</font></b></big></center>
+
+
+<form method="get" action="/" class="form-control">
+<input class="form-control" placeholder="Enter Your Name !" name="n">
+<input type="submit" class="btn btn-success " value="Create >>">
+</form>
+</div>
+
+</center>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</main>
+
+
+
+<a class="footerbtn" href="whatsapp://send?text=*<?php echo $_GET["n"]; ?> Ne Aapko Sabse Pahle* - http://wishingu.mghost.in/independence.php?n=<?php echo str_replace(" ","_",$_GET["n"]); ?> - Click Karke Dekho"><img width="20px" height="20px" src="/img/eid/wtsp.svg"/> SHARE ON WHATSAPP <img width="20px" height="20px" src="/img/eid/wtsp.svg"/></a>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+  
+<div style="margin:1em;">
+</div>
+</body>
+</html>
